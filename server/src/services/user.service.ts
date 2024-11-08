22b9be68221c2a459a9583551924bd7cdc80971a
takonguina <tommy.akonguina@yahoo.fr>
2024-11-08 18:29:59 +0100

    ta/server : module user - register service

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,6 +1,6 @@
 import * as bcrypt from 'bcryptjs';
 import { User } from 'src/models/user.model';
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateUserDto } from 'src/dto/create-user.dto';
 
@@ -12,10 +12,15 @@ export class UserService {
   async createUser(CreateUserDto: CreateUserDto): Promise<User> {
     const { email, password, firstName, lastName } = CreateUserDto;
 
+    // Check if all required fields are provided
+    if (!firstName || !lastName || !email || !password) {
+      throw new BadRequestException('Missing required fields');
+    }
+
     // Check if the user already exists
     const existingUser = await this.userModel.findOne({ where: { email } });
     if (existingUser) {
-      throw new Error('User already exists');
+      throw new BadRequestException('User already exists');
     }
 
     // Hash the password
