8840f7f49f113baf86231197e838ea9b7e47b3b7
takonguina <tommy.akonguina@yahoo.fr>
2024-11-11 22:09:58 +0100

    ta/server : add jwtmodule to task module

--- server/src/modules/task.module.ts
+++ server/src/modules/task.module.ts
@@ -5,9 +5,16 @@ import { TaskController } from 'src/controllers/task.controller';
 import { isAuthorizedGuard } from 'src/guards/is-authorized.guard';
 import { IsAuthenticatedGuard } from 'src/guards/is-authenticated.guard';
 import { TaskService } from 'src/services/task.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Task])],
+  imports: [
+    SequelizeModule.forFeature([Task]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [TaskController],
   providers: [TaskService, IsAuthenticatedGuard, isAuthorizedGuard],
 })
