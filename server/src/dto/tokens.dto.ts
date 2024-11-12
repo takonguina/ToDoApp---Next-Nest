2f6101fa0f845bdfbacb175d2b42a1244872b50e
takonguina <tommy.akonguina@yahoo.fr>
2024-11-12 15:13:53 +0100

    ta/server - add refresh token logic to auth

---
+++ server/src/dto/tokens.dto.ts
@@ -0,0 +1,4 @@
+export class TokensDto {
+  refreshToken: string;
+  accessToken: string;
+}
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -10,6 +10,7 @@ import { InjectModel } from '@nestjs/sequelize';
 import { LoginDto } from 'src/dto/login.dto';
 import { User } from 'src/models/user.model';
 import { RegisterDto } from 'src/dto/register.dto';
+import { TokensDto } from 'src/dto/tokens.dto';
 
 @Injectable()
 export class AuthService {
@@ -19,7 +20,7 @@ export class AuthService {
   ) {}
 
   // Register new user
-  async register(registerDto: RegisterDto): Promise<User> {
+  async register(registerDto: RegisterDto): Promise<TokensDto> {
     const { email, password, firstName, lastName } = registerDto;
 
     // Check if all required fields are provided
@@ -44,11 +45,23 @@ export class AuthService {
       lastName,
     });
 
-    return newUser;
+    // Generate JWT tokens
+    const accessToken = this.jwtService.sign({ id: newUser.id });
+    const refreshToken = this.jwtService.sign(
+      { id: newUser.id },
+      { expiresIn: '7d' },
+    );
+
+    // Hash the refreshtoken
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+    // Save the refreshtoken
+    await newUser.update({ refreshToken: hashedRefreshToken });
+
+    return { accessToken, refreshToken };
   }
 
   // Login user
-  async login(loginDto: LoginDto): Promise<{ token: string }> {
+  async login(loginDto: LoginDto): Promise<TokensDto> {
     const { email, password } = loginDto;
 
     // Check if all required fields are provided
@@ -68,9 +81,72 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    // Generate JWT token
-    const token = this.jwtService.sign({ id: user.id });
+    // Generate JWT tokens
+    const accessToken = this.jwtService.sign({ id: user.id });
+    const refreshToken = this.jwtService.sign(
+      { id: user.id },
+      { expiresIn: '7d' },
+    );
 
-    return { token };
+    // Hash the refreshtoken
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+
+    // Save the refreshtoken
+    await user.update({ refreshToken: hashedRefreshToken });
+
+    return { accessToken, refreshToken };
+  }
+
+  // Refresh tokens
+  async refresh(authorization: string): Promise<TokensDto> {
+    // Check if the authorization header is provided
+    if (!authorization) {
+      throw new UnauthorizedException('No authorization header found');
+    }
+
+    // Extract the token from the authorization header
+    const token = authorization.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('Invalid refresh token ');
+    }
+
+    // Verify the token
+    const payload = this.jwtService.verify(token, {
+      secret: process.env.JWT_SECRET,
+    });
+    if (!payload) {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+
+    // Check if the user exists
+    // Find by primary key
+    const user = await this.userModel.findByPk(payload.id);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    // Check if the refreshtoken is the same
+    const isRefreshTokenCorrect = await bcrypt.compare(
+      token,
+      user.refreshToken,
+    );
+    if (!isRefreshTokenCorrect) {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+
+    // Generate new JWT tokens
+    const accessToken = this.jwtService.sign({ id: user.id });
+    const refreshToken = this.jwtService.sign(
+      { id: user.id },
+      { expiresIn: '30d' },
+    );
+
+    // Hash the refreshtoken
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+
+    // Save the refreshtoken
+    await user.update({ refreshToken: hashedRefreshToken });
+
+    return { accessToken, refreshToken };
   }
 }
