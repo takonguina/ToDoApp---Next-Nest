afe5e6ee78e8439dc84b4b5f60dd2d54537d4a33
takonguina <tommy.akonguina@yahoo.fr>
2024-11-13 19:49:38 +0100

    ta/client - button & modal createTaskList

--- client/src/components/homePage/leftSideBar/LeftSideBar.jsx
+++ client/src/components/homePage/leftSideBar/LeftSideBar.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoIosAddCircle } from "react-icons/io";
 
 import List from "./List";
 import Logout from "./Logout";
+import CreateListModal from "../../modal/CreateListModal";
 
 const LeftSideBar = ({
   tasklists,
@@ -11,6 +13,11 @@ const LeftSideBar = ({
   handleDeleteTaskList,
 }) => {
   const [isOpen, setIsOpen] = useState(true);
+
+  // Modal
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
   return (
     <div
       className={`flex flex-col justify-between transition-all duration-300 ease-in-out rounded-xl m-4 p-4 h-[calc(100%-2rem)] bg-zinc-200 ${
@@ -36,6 +43,12 @@ const LeftSideBar = ({
           handleDeleteTaskList={handleDeleteTaskList}
         />
       )}
+      <IoIosAddCircle
+        size={36}
+        className="cursor-pointer"
+        onClick={() => openModal()}
+      />
+      <CreateListModal isOpen={showModal} onClose={closeModal} />
       <Logout isOpen={isOpen} />
     </div>
   );
